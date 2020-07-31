8c6479ee862603107d00441fdfd11958284fb8ac
rafael ubuntu <ryikeda@outlook.com>
2020-07-31 19:32:21 +0900

    created GET /users route

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.get("/", (req, res, next) => {
 });
 
 // app.use("/auth", authRoutes);
-// app.use("/users", userRoutes);
+app.use("/users", userRoutes);
 // app.use("/messages", messageRoutes);
 
 /** 404 handler */
--- models/user.js
+++ models/user.js
@@ -1,29 +1,37 @@
 /** User class for message.ly */
+const db = require("../db");
+const bcript = require("bcrypt");
+const ExpressError = require("../expressError");
 
-
+const { BCRYPT_WORK_FACTOR } = require("../config");
 
 /** User of the site. */
 
 class User {
-
   /** register new user -- returns
    *    {username, password, first_name, last_name, phone}
    */
 
-  static async register({username, password, first_name, last_name, phone}) { }
+  static async register({ username, password, first_name, last_name, phone }) {}
 
   /** Authenticate: is this username/password valid? Returns boolean. */
 
-  static async authenticate(username, password) { }
+  static async authenticate(username, password) {}
 
   /** Update last_login_at for user */
 
-  static async updateLoginTimestamp(username) { }
+  static async updateLoginTimestamp(username) {}
 
   /** All: basic info on all users:
    * [{username, first_name, last_name, phone}, ...] */
 
-  static async all() { }
+  static async all() {
+    const result = await db.query(`
+    SELECT username, first_name, last_name, phone
+    FROM users
+    ORDER BY username`);
+    return result.rows;
+  }
 
   /** Get: get user by username
    *
@@ -34,7 +42,7 @@ class User {
    *          join_at,
    *          last_login_at } */
 
-  static async get(username) { }
+  static async get(username) {}
 
   /** Return messages from this user.
    *
@@ -44,7 +52,7 @@ class User {
    *   {username, first_name, last_name, phone}
    */
 
-  static async messagesFrom(username) { }
+  static async messagesFrom(username) {}
 
   /** Return messages to this user.
    *
@@ -54,8 +62,7 @@ class User {
    *   {id, first_name, last_name, phone}
    */
 
-  static async messagesTo(username) { }
+  static async messagesTo(username) {}
 }
-
 
 module.exports = User;
